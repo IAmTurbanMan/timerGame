5e4a3b63be38542336055f71e3d93b214af5b30a
IAmTurbanMan <rashidul.kader@gmail.com>
2020-08-23 15:01:56 -0700

    worked on selection 1: Main game

--- timerGame.py
+++ timerGame.py
@@ -4,12 +4,13 @@ import os
 import pickle
 import datetime
 from time import sleep
-from random import randit
+from random import randint
 from operator import itemgetter
 
 selection = ''
 players = []
-high_scores = []
+globalHighScores = []
+currentScores = []
 
 def clear():
 	os.system('clear')
@@ -28,7 +29,7 @@ def menu():
 ***************************
 	""")
 
-while selection != '6':
+while True:
 
 	clear()
 	menu()
@@ -39,6 +40,45 @@ while selection != '6':
 		clear()
 		if len(players) < 2:
 			print ('There must be at least 2 players')
+			sleep (2)
+			menu()
+
+		else:
+			for x in range (len(players)):
+				print ("""
+
+		**********************{}************************
+				""".format(players[x]))
+
+				sleep (randint(1, 10))
+				startTime = datetime.datetime.now()
+
+				input("""
+		************************************************
+		************************************************
+		**********************Press*********************
+		**********************Enter*********************
+		************************************************
+		************************************************
+				""")
+				endTime = datetime.datetime.now()
+				timeDiff = endTime - startTime
+				diffStr = str(timeDiff)
+
+				print (players[x] + ':' + diffStr)
+				currentScores.append((players[x], diffStr))
+				globalHighScores.append((players[x], diffStr))
+
+				currentScores = sorted(currentScores, key=itemgetter(1), reverse=False)
+				globalHighScores = sorted(globalHighScores, key=itemgetter(1), reverse=False)[:10]
+				print (globalHighScores)
+				sleep(5)
+				clear()
+
+		winnerName = currentScores[0][0]
+		winnerTime = currentScores[0][1]
+		print ('The winner of this round is {} with a time of {}'.format(winnerName,winnerTime))
+		sleep(5)
 
 	if selection == '2':
 		clear()
