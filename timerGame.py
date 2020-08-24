4cdb415b625a2f20a453b34424ae0305246a09e9
IAmTurbanMan <rashidul.kader@gmail.com>
2020-08-24 13:40:56 -0700

    added comments

--- timerGame.py
+++ timerGame.py
@@ -7,11 +7,14 @@ from time import sleep
 from random import randint
 from operator import itemgetter
 
+#define vairables and lists
 selection = 0
 players = []
 currentScores = []
 globalHighScores = []
 
+#check if highscores.txt exists then load the data from the file
+#into globalHighScores list
 if os.path.isfile('highscores.txt'):
 	with open('highscores.txt','rb') as f:
 		globalHighScores = pickle.load(f)
@@ -40,6 +43,8 @@ while True:
 
 	clear()
 	menu()
+
+	#error handling for incorrect entries
 	try:
 		selection = int(input('Selection: '))
 	except ValueError:
@@ -52,6 +57,16 @@ while True:
 		print ('Please enter a valid selection')
 		moveOn()
 
+	#block for main game
+	#clears currentScores list
+	#verifies that there are two or more players
+	#loops the game for the number of players
+	#after printing player name at top, will wait for a random number of seconds between 3 and 13
+	#records the time as a datetime object when the "Press Enter" message pops up, waits for player to press enter
+	#records the time as a datetime object when the player presses enter
+	#calculates time difference and then enters player name and time difference into currentScores list
+	#currentScores list is sorted from fastest to slowest time and the winner is whichever player is index[0] on that list
+	#also populates the globalHighScores list which is sorted in a similar manner, but will only keep the top 10
 	if selection == 1:
 		clear()
 		currentScores.clear()
@@ -75,8 +90,8 @@ while True:
 				input("""
 		************************************************
 		************************************************
-		**********************Press*********************
-		**********************Enter*********************
+		********************  Press  *******************
+		********************  Enter  *******************
 		************************************************
 		************************************************
 				""")
@@ -98,6 +113,7 @@ while True:
 		print ('The winner of this round is {} with a time of {}'.format(winnerName,winnerTime))
 		moveOn()
 
+	#player name entry
 	if selection == 2:
 		clear()
 		playerName = input('Player name: ')
@@ -105,6 +121,7 @@ while True:
 		print ('{} is now playing'.format(playerName))
 		moveOn()
 
+	#player name deletion, with error handling
 	if selection == 3:
 		clear()
 		exitPlayer = input('Player to remove: ')
@@ -118,6 +135,7 @@ while True:
 		print ('{} is no longer part of the game'.format(exitPlayer))
 		moveOn()
 
+	#check currentScores list
 	if selection == 4:
 		clear()
 		for x in range (len(currentScores)):
@@ -125,12 +143,16 @@ while True:
 
 		moveOn()
 
+	#check globalHighScores list
 	if selection == 5:
 		clear()
 		for x in range (len(globalHighScores)):
 			print ('{}	{}'.format(globalHighScores[x][0],globalHighScores[x][1]))
 		moveOn()
 
+	#writes globalHighScores list to highscores.txt
+	#thanks players for playing
+	#exits the program
 	if selection == 6:
 		clear()
 		with open ('highscores.txt','wb') as f:
